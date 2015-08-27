79deb9d72b3a2e34671443b1d7e8ae9d2653ba56
lispro06 <lispro06@gmail.com>
2015-08-27 09:28:23 +0900

    Update and rename ch-1.clj to ch03-1.clj

--- ch03-1.clj
+++ ch03-1.clj
@@ -244,3 +244,215 @@
   [(count t) (count d)])
 ;= [12 99999988]
 
+
+;(let [[t d] (split-with #(< % 12) (range 1e8))]
+;  [(count t) (count d)])
+;= [12 99999988]
+
+(def m {:a 1, :b 2, :c 3})
+;= #'user/m
+(get m :b)
+;= 2
+(get m :d)
+;= nil
+(get m :d "not-found")
+;= "not-found"
+(assoc m :d 4)
+;= {:a 1, :b 2, :c 3, :d 4}
+(dissoc m :b)
+;= {:a 1, :c 3}
+
+(assoc m
+  :x 4
+  :y 5
+  :z 6)
+;= {:z 6, :y 5, :x 4, :a 1, :c 3, :b 2}
+(dissoc m :a :c)
+;= {:b 2}
+
+(def v [1 2 3])
+;= #'user/v
+(get v 1)
+;= 2
+(get v 10)
+;= nil
+(get v 10 "not-found")
+;= "not-found"
+(assoc v
+  1 4
+  0 -12
+  2 :p)
+;= [-12 4 :p]
+
+(assoc v 3 10)
+;= [1 2 3 10]
+
+(get #{1 2 3} 2)
+;= 2
+(get #{1 2 3} 4)
+;= nil
+(get #{1 2 3} 4 "not-found")
+;= "not-found"
+
+(when (get #{1 2 3} 2)
+  (println "it contains `2`!"))
+; it contains `2`!
+
+(contains? [1 2 3] 0)
+;= true
+(contains? {:a 5 :b 6} :b)
+;= true
+(contains? {:a 5 :b 6} 42)
+;= false
+(contains? #{1 2 3} 1)
+;= true
+
+ (contains? [1 2 3] 3)
+;= false
+(contains? [1 2 3] 2)
+;= true
+(contains? [1 2 3] 0)
+;= true
+
+(get "Clojure" 3)
+;= \j
+(contains? (java.util.HashMap.) "not-there")
+;= false
+(get (into-array [1 2 3]) 0)
+;= 1
+
+(get {:ethel nil} :lucy)
+;= nil
+(get {:ethel nil} :ethel)
+;= nil
+
+(find {:ethel nil} :lucy)
+;= nil
+(find {:ethel nil} :ethel)
+;= [:ethel nil]
+
+(if-let [e (find {:a 5 :b 6} :a)]
+  (format "found %s => %s" (key e) (val e))
+  "not found")
+;= "found :a => 5"
+(if-let [[k v] (find {:a 5 :b 6} :a)]
+  (format "found %s => %s" k v)
+  "not found")
+;= "found :a => 5"
+
+(nth [:a :b :c] 2)
+;= :c
+(get [:a :b :c] 2)
+;= :c
+;(nth [:a :b :c] 3)
+;= java.lang.IndexOutOfBoundsException
+(get [:a :b :c] 3)
+
+;= nil
+;(nth [:a :b :c] -1)
+;= java.lang.IndexOutOfBoundsException
+(get [:a :b :c] -1)
+;= nil
+
+(nth [:a :b :c] -1 :not-found)
+;= :not-found
+(get [:a :b :c] -1 :not-found)
+;= :not-found
+
+(get 42 0)
+;= nil
+;(nth 42 0)
+;= java.lang.UnsupportedOperationException: nth not supported on this type: Long
+
+
+(conj '() 1)
+;= (1)
+(conj '(2 1) 3)
+;= (3 2 1)
+(peek '(3 2 1))
+;= 3
+(pop '(3 2 1))
+;= (2 1)
+(pop '(1))
+;= ()
+
+(conj [] 1)
+;= [1]
+(conj [1 2] 3)
+;= [1 2 3]
+(peek [1 2 3])
+;= 3
+(pop [1 2 3])
+;= [1 2]
+(pop [1])
+;= []
+
+(get #{1 2 3} 2)
+;= 2
+(get #{1 2 3} 4)
+;= nil
+(get #{1 2 3} 4 "not-found")
+;= "not-found"
+
+(disj #{1 2 3} 3 1)
+;= #{2}
+
+(def sm (sorted-map :z 5 :x 9 :y 0 :b 2 :a 3 :c 4))
+;= #'user/sm
+sm
+;= {:a 3, :b 2, :c 4, :x 9, :y 0, :z 5}
+(rseq sm)
+;= ([:z 5] [:y 0] [:x 9] [:c 4] [:b 2] [:a 3])
+(subseq sm <= :c)
+;= ([:a 3] [:b 2] [:c 4])
+(subseq sm > :b <= :y)
+;= ([:c 4] [:x 9] [:y 0])
+(rsubseq sm > :b <= :y)
+;= ([:y 0] [:x 9] [:c 4])
+
+(compare 2 2)
+;= 0
+(compare "ab" "abc")
+;= -1
+(compare ["a" "b" "c"] ["a" "b"])
+;= 1
+(compare ["a" 2] ["a" 2 0])
+;= -1
+
+(sort < (repeatedly 10 #(rand-int 100)))
+;= (12 16 22 23 41 42 61 63 83 87)
+(sort-by first > (map-indexed vector "Clojure"))
+;= ([6 \e] [5 \r] [4 \u] [3 \j] [2 \o] [1 \l] [0 \C])
+
+(sorted-map-by compare :z 5 :x 9 :y 0 :b 2 :a 3 :c 4)
+;= {:a 3, :b 2, :c 4, :x 9, :y 0, :z 5}
+(sorted-map-by (comp - compare) :z 5 :x 9 :y 0 :b 2 :a 3 :c 4)
+;= {:z 5, :y 0, :x 9, :c 4, :b 2, :a 3}
+
+(defn magnitude
+  [x]
+  (-> x Math/log10 Math/floor))
+;= #'user/magnitude
+(magnitude 100)
+;= 2.0
+(magnitude 100000)
+;= 5.0
+
+(defn compare-magnitude
+  [a b]
+  (- (magnitude a) (magnitude b)))
+((comparator compare-magnitude) 10 10000)
+;= -1
+((comparator compare-magnitude) 100 10)
+;= 1
+((comparator compare-magnitude) 10 75)
+;= 0
+
+(sorted-set-by compare-magnitude 10 1000 500)
+;= #{10 500 1000}
+(conj *1 600)      
+;= #{10 500 1000}
+(disj *1 750)      
+;= #{10 1000}
+(contains? *1 1239)
+;= true
